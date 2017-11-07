6d8d09ac08e40418c589909c14eba94d550bf1eb
Philipp Steingrebe <development@steingrebe.de>
2017-11-07 11:09:52 +0100

    Changed indentation from Tab to Spaces New tests covering the new "before" and "after" middleware. Tests targeting exeption handling have been removed.

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -5,6 +5,7 @@ use Everest\Http\Route;
 use Everest\Http\Requests\ServerRequest;
 use Everest\Http\Requests\Request;
 use Everest\Http\Responses\Response;
+use Everest\Http\Responses\ResponseInterface;
 use Everest\Http\Uri;
 
 // Load middleware class
@@ -15,174 +16,170 @@ require __DIR__ . '/fixtures/Middleware.php';
  */
 class RouterTest extends \PHPUnit_Framework_TestCase {
 
-	public function setUp()
-	{
-		// Mock request
-		$this->request = $this->getMockBuilder(ServerRequest::CLASS)
-			->disableOriginalConstructor()
-			->getMock();
-		$this->request->method('getUri')->willReturn(Uri::from('http://steingrebe.de/prefix/test?some=value#hash'));
-		$this->request->method('isMethod')->willReturn(true);
-	}
+  public function setUp()
+  {
+    // Mock request
+    $this->request = $this->getMockBuilder(ServerRequest::CLASS)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $this->request->method('getUri')->willReturn(Uri::from('http://steingrebe.de/prefix/test?some=value#hash'));
+    $this->request->method('isMethod')->willReturn(true);
+  }
 
   /**
-   * @expectedException        Exception
-   * @expectedExceptionMessage No matching route found
+   * @expectedException        \Everest\Http\HttpException
+   * @expectedExceptionCode 404
    */
-	public function testEmptyResultLeedsToException()
-	{
-		$test = $this;
-		$router = new Router;
-		$router->get('prefix/test', function() use ($test) {
-		})->handle($this->request);
-	}
+
+  public function testEmptyResultLeedsToException()
+  {
+    $test = $this;
+    $router = new Router;
+    $router->get('prefix/test', function() use ($test) {
+    })->handle($this->request);
+  }
 
 
-	public function testRoutingGet()
-	{
-		$test = $this;
-		$router = new Router;
-		$router->get('prefix/test', function() use ($test) {
-			$test->assertTrue(true);
-			return 'Response!';
-		})->handle($this->request);
-	}
+  public function testRoutingGet()
+  {
+    $test = $this;
+    $router = new Router;
+    $router->get('prefix/test', function() use ($test) {
+      $test->assertTrue(true);
+      return 'Response!';
+    })->handle($this->request);
+  }
 
-	public function testRoutingPost()
-	{
-		$test = $this;
-		$router = new Router;
-		$router->post('prefix/test', function() use ($test) {
-			$test->assertTrue(true);
-			return 'Response!';
-		})->handle($this->request);
-	}
+  public function testRoutingPost()
+  {
+    $test = $this;
+    $router = new Router;
+    $router->post('prefix/test', function() use ($test) {
+      $test->assertTrue(true);
+      return 'Response!';
+    })->handle($this->request);
+  }
 
-	public function testRoutingRequest()
-	{
-		$test = $this;
-		$router = new Router;
-		$router->request('prefix/test', ServerRequest::HTTP_ALL, function() use ($test) {
-			$test->assertTrue(true);
-			return 'Response!';
-		})->handle($this->request);
-	}
+  public function testRoutingRequest()
+  {
+    $test = $this;
+    $router = new Router;
+    $router->request('prefix/test', ServerRequest::HTTP_ALL, function() use ($test) {
+      $test->assertTrue(true);
+      return 'Response!';
+    })->handle($this->request);
+  }
 
-	public function testContext()
-	{
-		$test = $this;
-		$router = new Router(['debug' => true]);
-		$router->context('prefix', function(Router $router) use ($test) {
-			$router->get('test', function() use ($test) {
-				$test->assertTrue(true);
-				return 'Response!';
-			});
-		})->handle($this->request);
-	}
+  public function testContext()
+  {
+    $test = $this;
+    $router = new Router(['debug' => true]);
+    $router->context('prefix', function(Router $router) use ($test) {
+      $router->get('test', function() use ($test) {
+        $test->assertTrue(true);
+        return 'Response!';
+      });
+    })->handle($this->request);
+  }
 
-	public function testNestedContext()
-	{
-		$test = $this;
-		$router = new Router(['debug' => true]);
-		$router->context('prefix', function(Router $router) use ($test) {
-			$router->context('test', function(Router $router) use ($test) {
-				$router->get('/', function() use ($test) {
-					$test->assertTrue(true);
-					return 'Response!';
-				});
-			});
-		})->handle($this->request);
-	}
+  public function testNestedContext()
+  {
+    $test = $this;
+    $router = new Router(['debug' => true]);
+    $router->context('prefix', function(Router $router) use ($test) {
+      $router->context('test', function(Router $router) use ($test) {
+        $router->get('/', function() use ($test) {
+          $test->assertTrue(true);
+          return 'Response!';
+        });
+      });
+    })->handle($this->request);
+  }
 
-	public function testMiddleware()
-	{
-		$test = $this;
-		$router = new Router(['debug' => true]);
-		$router
-			// Closure middleware
-			->middleware(function(Closure $next, ...$args) {
-				return $next('test', ...$args);
-			})
-			// Object middleware
-			->middleware(new Middleware('test'))
-			->get('prefix/test', function($arg1, $arg2, $request, $params) use ($test) {
-				$this->assertEquals('test', $arg1);
-				$this->assertEquals('test', $arg2);
-				$this->assertInstanceOf(Request::CLASS, $request);
-				$this->assertEquals([], $params);
+  public function testMiddleware()
+  {
+    $test = $this;
+    $router = new Router(['debug' => true]);
+    $result = 
+    $router
+      // Closure middleware
+      ->before(function(Closure $next, ...$args) {
+        return $next('test', ...$args);
+      })
+      // Object middleware
+      ->before(new Middleware('test'))
+      ->after(function(Closure $next, $response){
+        return $next($response . 'After');
+      })
+      ->get('prefix/test', function($arg1, $arg2, $request, $params) {
+        $this->assertEquals('test', $arg1);
+        $this->assertEquals('test', $arg2);
+        $this->assertInstanceOf(Request::CLASS, $request);
+        $this->assertEquals([], $params);
 
-				return 'Middleware Response!';
-			})
-			->handle($this->request);
-	}
+        return 'Response';
+      })
+      ->handle($this->request);
 
-	public function testNestedMiddleware()
-	{
-		$router = new Router;
-		$router
-			// Root context middleware
-			->middleware(new Middleware('arg1'))
-			->context('prefix', function(Router $router) {
-				$router
-					// Sub context middleware
-					->middleware(new Middleware('arg2'))
-					->get('test', function($arg1, $arg2, $request, $params) {
-							$this->assertEquals('test', $arg1);
-							$this->assertEquals('test', $arg2);
-							$this->assertInstanceOf(Request::CLASS, $request);
-							$this->assertEquals([], $params);
-							return 'Middleware Response!';
-					});
-			});
-	}
+      $this->assertInstanceOf(ResponseInterface::CLASS, $result);
+      $this->assertSame('ResponseAfter', (string)$result->getBody());
+  }
 
-	public function testDefaultHandlerOnEmptyResult()
-	{
-		$test = $this;
-		(new Router)
-			->otherwise(function() use ($test) {
-				// Default handler is called
-				$test->assertTrue(true);
-				return 'not-empty-result';
-			})
-			->get('prefix/test', function() use ($test) {
-				// Handler gets called but has no result
-				$test->assertTrue(true);
-			})
-			->handle($this->request);
-	}
+  public function testNestedMiddleware()
+  {
+    $router = new Router;
+    $router
+      // Root context middleware
+      ->before(new Middleware('arg1'))
+      ->context('prefix', function(Router $router) {
+        $router
+          // Sub context middleware
+          ->before(new Middleware('arg2'))
+          ->get('test', function($arg1, $arg2, $request, $params) {
+              $this->assertEquals('test', $arg1);
+              $this->assertEquals('test', $arg2);
+              $this->assertInstanceOf(Request::CLASS, $request);
+              $this->assertEquals([], $params);
+              return 'Middleware Response!';
+          });
+      });
+  }
 
-	public function testDefaultHandlerOnMissingMatch()
-	{
-		$test = $this;
-		$response = 
-		(new Router)
-			->otherwise(function() use ($test) {
-				// Default handler is called
-				$test->assertTrue(true);
-				return 'not-empty-result';
-			})
-			->get('this/does/not/match', function() use ($test) {
-				// Handler gets called never called!
-				$test->assertTrue(false);
-			})
-			->handle($this->request);
+  public function testDefaultHandlerOnEmptyResult()
+  {
+    $calledHandler = false;
+    $calledDefault = false;
+    $response = (new Router)
+      ->otherwise(function() use (&$calledDefault) {
+        $calledDefault = true;
+        return 'not-empty-result';
+      })
+      ->get('prefix/test', function() use (&$calledHandler) {
+        $calledHandler = true;
+      })
+      ->handle($this->request);
 
-		$this->assertInstanceOf(Response::CLASS, $response);
-	}
+    $this->assertTrue($calledHandler && $calledDefault);
+  }
 
-	public function testDefaultHandlerOnException()
-	{
-		$test = $this;
-		(new Router)
-			->otherwise(function($request, $exception) use ($test) {
-				$test->assertInstanceOf(Exception::CLASS, $exception);
-				$test->assertEquals('Route error', $exception->getMessage());
-				return 'not-empty-result';
-			})
-			->get('prefix/test', function() use ($test) {
-				throw new Exception('Route error');
-			})
-			->handle($this->request);
-	}
+  public function testDefaultHandlerOnMissingMatch()
+  {
+    $calledHandler = false;
+    $calledDefault = false;
+    $response = (new Router)
+      ->otherwise(function() use (&$calledDefault) {
+        $calledDefault = true;
+        return 'not-empty-result';
+      })
+      ->get('this/does/not/match', function() use (&$calledHandler) {
+        $calledHandler = true;
+      })
+      ->handle($this->request);
+
+    $this->assertTrue($calledDefault);
+    $this->assertFalse($calledHandler);
+
+    $this->assertInstanceOf(ResponseInterface::CLASS, $response);
+    $this->assertSame('not-empty-result', (string)$response->getBody());
+  }
 }
